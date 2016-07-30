64274f06231fec3598d13e1bace0c145ddc321ad
davidvujic <david@vujic.se>
2016-07-30 23:08:36 +0200

    moved greeting to render init function

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ var logic = gameLogicMaker();
 game = gameMaker(config, logic, view);
 
 var grid = gridMaker(config).create();
+
+view.init();
 view.render(grid);
 
 document.querySelector('#start-game').addEventListener('click', function () {
@@ -36,41 +38,3 @@ document.querySelector('#start-game').addEventListener('click', function () {
 
     game.play(grid);
 });
-
-(function () {
-    var greeting = "Greetings Dr Function.";
-    var question = 'How about a nice game of Tic-Tac-Func?';
-    var i = 0;
-
-    function writeGreeting() {
-        document.querySelector('#greeting-area').innerHTML += greeting[i];
-        i += 1;
-
-        if (i < greeting.length) {
-            setTimeout(writeGreeting, 50);
-        } else {
-            i = 0;
-            setTimeout(writeQuestion, 1000);
-
-        }
-    }
-
-    function writeQuestion() {
-        document.querySelector('#question-area').innerHTML += question[i];
-        i += 1;
-
-        if (i < question.length) {
-            setTimeout(writeQuestion, 50);
-        } else {
-            setTimeout(function () {
-                document.querySelector('#players-board').setAttribute('class', '');
-                document.querySelector('#user-actions').setAttribute('class', '');
-
-                setTimeout(function () {
-                    document.querySelector('#board-area').setAttribute('class', '');
-                }, 500);
-            }, 500);
-        }
-    }
-    setTimeout(writeGreeting, 1000);
-}());
